3791759bc7196f12585053e42e06b24ca82ef34b
mhmtvrl <mhmtvrl1731@gmail.com>
2015-12-01 04:18:23 -0500

    Constants.java have been removed

--- src/main/java/com/abc/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.abc;
-
-public class Constants {
-	public static final double CHECKING_ACCOUNT_INTEREST_RATE = 0.001;
-	public static final double MULTI_SAVINGS_ACCOUNT_LOWER_BOUND = 1000;
-	
-}
